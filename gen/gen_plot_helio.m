d6a2e7bfe70a851752589f5046be6c28e7763d64
Pjer <pjer1316@icloud.com>
2017-08-03 16:02:36 +0800

    complete mesh

--- gen/gen_plot_helio.m
+++ gen/gen_plot_helio.m
@@ -3,22 +3,35 @@
 % imagesc plot
 % date : 2017-07-29 23:02:35
 
-function [X,Y] = gen_plot_helio(radius, bangle, roll, gridn)
+function [X,Y] = gen_plot_helio(radius, bangle, roll, gridn,center_x,center_y)
     hold on
     
     if ~exist('gridn','var')  , gridn = 15; end
     if ~exist('roll','var')   , roll  = 0 ; end
     if ~exist('radius','var') , radius= 1000;end
     if ~exist('bangle','var') , bangle=5; end
+    if ~exist('center_x','var') , center_x=0; end
+    if ~exist('center_y','var') , center_y=0; end
     
-    varc = -90 + gridn*linspace(0,ceil(180/gridn),(ceil(180/gridn+1)));
-    varr = -90 + gridn*linspace(0,ceil(180/gridn),(ceil(180/gridn+1)));
+    varc = -90 + linspace(0,180,gridn);
+    varr = -90 + linspace(0,180,gridn);
     
-    [LAT, LON] = meshgrid(varc, varr); 
+    for num_i = 1:length(varc)
+        lon_t = varr(1):0.2:varr(end);
+        lat_t = varc(num_i)*ones(1,length(lon_t));
+        [xt,yt] = hel2arcmin(lat_t,lon_t,radius,bangle);
+        plot(center_x+xt*60,center_y+yt*60,'color',0.6*[1,1,1]);
+    end
+    
+    for num_i = 1:length(varr)
+        lat_t = varc(1):0.2:varc(end);
+        lon_t = varr(num_i)*ones(1,length(lon_t));
+        [xt,yt] = hel2arcmin(lat_t,lon_t,radius,bangle);
+        plot(center_x+xt*60,center_y+yt*60,'color',0.6*[1,1,1]);
+    end
+    
     if roll>eps
         do_roll(roll);
     end
-    [X,Y] = hel2arcmin(LAT,LON,radius,bangle);
-    plot(X(:),Y(:),'.');
     hold off
 end
--- srads_plot_nobeyama/srads_plot_norh_img.m
+++ srads_plot_nobeyama/srads_plot_norh_img.m
@@ -1,9 +1,13 @@
-% author : pjer
+% author : pjzhang
 % acroding to Nobeyama offical document
 % date : 2017-08-01 23:02:35 
 % plot .fit data
 
 function [data,info] = srads_plot_norh_img(fname)
+    
+    % add general toolset in current path sothe functions are available
+    path(path,'../gen');
+
     if nargin < 1
         fname = '/Users/jing/Desktop/DATA/norh/ipa050803_024206';
         % for debug just ignore it 
@@ -30,12 +34,27 @@ function [data,info] = srads_plot_norh_img(fname)
     key_wd_id(all(emptyCells,2),:) = [];
         
     info = cell2struct(key_wd_val,key_wd_id,1);
+    
+    if ~exist('gridn','var'), gridn = 4096/info.NAXIS1;  end
+    
     th = 0:0.001:2*pi;
     imagesc(data)
     colormap jet
     colorbar
     hold on
     plot(info.SOLR*cos(th)-info.CRVAL1+info.CRPIX1,info.SOLR*sin(th)-info.CRVAL2+info.CRPIX2,'w')
+    
+    bangle = info.SOLB;
+    radius = info.SOLR;
+    center_x = -info.CRVAL1+info.CRPIX1;
+    center_y = -info.CRVAL2+info.CRPIX2;
+    
+    % overlay the grid to the map
+    gen_plot_helio(radius, bangle, 0 , gridn,center_x,center_y);
+    
     title([(info.ORIGIN),' ',info.TELESCOP,' ( ',info.OBS_FREQ,' ',info.POLARIZ,' )'],'Fontsize',16)
     text(2,info.NAXIS2*0.98 ,info.PROGNAME,'fontsize',14,'color','w')
+    text(2,info.NAXIS2*0.04 ,[info.DATE_OBS,'  ',info.TIME_OBS],'fontsize',14,'color','w')
+
+    
 end
