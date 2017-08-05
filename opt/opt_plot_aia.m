27da4e7972d7e1ae2baa342eeb68f1a138b34288
PJZhang <pjer1316@icloud.com>
2017-08-06 00:02:53 +0800

    add opt file

--- opt/opt_plot_aia.m
+++ opt/opt_plot_aia.m
@@ -1,5 +1,11 @@
-function opt_plot_aia()
-
+function [data,info] = opt_plot_aia(fname,posi,sizex,sizey)
+    if ~exist('sizex','var'), sizex = 0; end
+    if ~exist('sizey','var'), sizey = sizex; end
+    if ~exist('fname','var'), fname = 'aia.lev1.304A_2012-02-12T00_25_08.12Z.image_lev1.fits' ;end
     
-
+    data = fitsread(fname,'image');
+    info = fitsinfo(fname);
+    kw = info.Image.Keywords;
+    disp(kw)
+    imagesc(data)
 end
